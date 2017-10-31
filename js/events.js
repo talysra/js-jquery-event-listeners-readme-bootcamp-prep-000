7f9514a551ca34d50b7a49a8432f6ac44a47c96a
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-31 13:11:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,9 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form').on()
+  $('form').on('submit'), function() {
+    alert('Your form is going to be submitted now.')
+  }
 }
 
 $(document).ready(function(){
