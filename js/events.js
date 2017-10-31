676d82910ef592d20f50c4870c39831982923eb5
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-31 13:07:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,18 @@
 //define functions here
+function getIt() {
+  $('p').on('click'), alert("Hey!")
+}
+
+function frameIt() {
+  $('.tasty').on('load');
+}
+
+function pressIt() {
+  $('input').on('keydown')
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
 });
